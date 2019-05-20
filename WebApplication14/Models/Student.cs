b48d5672aef531c2a42a7529861ec89b5d1189b4
utvecklare.mattias@gmail.com <utvecklare.mattias@gmail.com>
2019-05-20 15:57:29 +0200

    20Mqj

---
+++ WebApplication14/Models/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ListOfStudents.Models
+{
+    public class Student
+    {
+        public Student()
+        {
+        }
+        public Student(string TeacherName, string FirstName, string LastName, string Course, int CourseID)
+        {
+            TecherName = teachername;
+            FirstName = firstname;
+            LastName = lastName;
+            Course = course;
+            CourseID = courseID;
+        }
+        public Student(int id, string TeacherName, string FirstName, string LastName, string Course, int Courseid)
+        {
+            ID= id;
+            TeacherName = teacherName;
+            FirstName = firstName;
+            LastName = lastName;
+            Course = course;
+            Courseid = courseid;
+
+        }
+        public int CourseID { get; }
+        public int ID{ get; set; }
+        public string TeacherName { get; }
+        public string FirstName { get; set; }
+        public string LatName { get; set; }
+        public string Course { get; }
+    }
+}
---
+++ WebApplication14/Models/Teacher.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ListOfStudents.Models
+{
+    public class Teacher
+    {
+        public Teacher()
+        {
+        }
+        public Teacher(string TeacherName, string FirstName, string LastName, string Course, int CourseID)
+        {
+            TecherName = teachername;
+            FirstName = firstname;
+            LastName = lastName;
+            Course = course;
+            CourseID = courseID;
+        }
+        public Teacher(int id, string TeacherName, string FirstName, string LastName, string Course, int Courseid)
+        {
+            ID= id;
+            TeacherName = teacherName;
+            FirstName = firstName;
+            LastName = lastName;
+            Course = course;
+            Courseid = courseid;
+
+        }
+        public int CourseID { get; set; }
+        public int ID{ get; set; }
+        public string TeacherName { get; }
+        public string FirstName { get; set; }
+        public string LatName { get; set; }
+        public string Course { get; set; }
+    }
+}
