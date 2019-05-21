ece0aef3f806d50d945bc230804c3577a3c892a2
utvecklare.mattias@gmail.com <utvecklare.mattias@gmail.com>
2019-05-21 16:24:11 +0200

    21maj 1625

--- WebApplication14/Models/Student.cs
+++ WebApplication14/Models/Student.cs
@@ -2,37 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 
-namespace ListOfStudents.Models
+namespace School.Models
 {
+    public class School
+    {
+    }
     public class Student
     {
-        public Student()
-        {
-        }
-        public Student(string TeacherName, string FirstName, string LastName, string Course, int CourseID)
-        {
-            TecherName = teachername;
-            FirstName = firstname;
-            LastName = lastName;
-            Course = course;
-            CourseID = courseID;
-        }
-        public Student(int id, string TeacherName, string FirstName, string LastName, string Course, int Courseid)
-        {
-            ID= id;
-            TeacherName = teacherName;
-            FirstName = firstName;
-            LastName = lastName;
-            Course = course;
-            Courseid = courseid;
+        public int id { get; set; }
+        public string name { get; set; }
+        public string CourseName { get; set;
+     
 
-        }
-        public int CourseID { get; }
-        public int ID{ get; set; }
-        public string TeacherName { get; }
-        public string FirstName { get; set; }
-        public string LatName { get; set; }
-        public string Course { get; }
-    }
-}
+
+    
+  
