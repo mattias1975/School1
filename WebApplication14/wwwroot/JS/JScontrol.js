e75a02d29eb78f647fba5736cc2b1fe37909105a
utvecklare.mattias@gmail.com <utvecklare.mattias@gmail.com>
2019-05-24 16:20:42 +0200

    wekend

--- WebApplication14/wwwroot/JS/JScontrol.js
+++ WebApplication14/wwwroot/JS/JScontrol.js
@@ -9,3 +9,20 @@ function Save(html_id, edit_url) {
     $.Save(html_url, edit_url)
     $('#' + html_id.Save(data));
 }
+
+function DeleteItem(html_id, edit_url) {
+    $.get(edit_url, function (data, status) {
+        $('#' + html_id).replaceWith(data);
+    });
+
+    function CreateStundent(create_url)
+    var student = {
+    }
+    $.post(create_url,
+        {
+            Name: $('#Name').val(),
+            Course: $('#CourseName').val(),
+            Email: $('#Email').val()
+        });
+    }
+    
