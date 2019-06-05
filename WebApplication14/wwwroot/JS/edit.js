e552b9d1c253802ddd96d848b7602f05cca32db4
utvecklare.mattias@gmail.com <utvecklare.mattias@gmail.com>
2019-06-05 12:38:30 +0200

    12:40

--- WebApplication14/wwwroot/JS/edit.js
+++ WebApplication14/wwwroot/JS/edit.js
@@ -1,12 +1,12 @@
 ﻿"use strict";
 
-function EditListItem(html_id, edit_url) {
+function EditStudent(html_id, edit_url) {
     $.get(edit_url, function (data, status) {
         $('#' + html_id).replaceWith(data);
     });
 }
 
-function SaveEditItem(html_id, student_id, edit_url) {
+function SaveEditStudent(html_id, student_id, edit_url) {
     var student = {
         Id: student_id,
         Name: $('#Name' + student_id).val(),
@@ -21,12 +21,8 @@ function SaveEditItem(html_id, student_id, edit_url) {
         });
 };
 
-function CreateItem(create_url) {
-    //var student = {
-
-    //}
-
-    //ajax post
+function CreateStudent(create_url) {
+  
     $.post(create_url,
         {
             Name: $('#name').val(),
@@ -42,21 +38,53 @@ function CreateItem(create_url) {
     // inject respone Student in list "id:studentlist"
 
 }
-function DeleteItem(html_id, edit_url) {
-
+function DeleteStudent(html_id, edit_url) {
     
-   
-
     $.get(edit_url, function (data, status) {
         $('#' + html_id).replaceWith(data);
         });
     }
+//Teacher
+function SaveEditTeacher(html_id, teacher_id, edit_url) {
+    var teacher = {
+        Id: teacher_id,
+        Name: $('#name' + teacher_id).val(),
+        Email: $('#email' + teacher_id).val()
+    }
+    $.post(edit_url,
+        {
+            teacher: teacher
+        }
+        , function (data, status) {
+            $('#' + html_id).replaceWith(data);
+        });
+};
+
+function CreateItem(create_url) {
+ 
+    $.post(create_url,
+        {
+            Name: $('#name').val(),
+            Email: $('#email').val()
+        }
+        , function (data, status) {
+
+            $("#teacherlist").append(data);
+        });
+
+
+
+    // inject respone Student in list "id:studentlist"
+
+}
+function DeleteTeacher(html_id, edit_url) {
+
+
+    $.get(edit_url, function (data, status) {
+        $('#' + html_id).replaceWith(data);
+    });
+}
 
     
 
 
-//function rrr (html_id, edit_url) {
-//    $.get(edit_url, function (data, status) {
-//        $('#' + html_id).replaceWith(data);
-//    });
-//};
